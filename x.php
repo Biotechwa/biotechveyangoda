4f29689b862b45fbb32ec8a77530264a5a5c4476
Biotech <theek1298@gmail.com>
2024-12-26 14:12:05 +0530

    Update x.php

--- x.php
+++ x.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
     // Validate credentials
-    if ($username == 'admin' && $password == 'admin123') {
+    if ($username == 'admin_btlab' && $password == 'admin_btlab') {
         echo 'Login successful';
     } else {
         echo 'Invalid credentials';
